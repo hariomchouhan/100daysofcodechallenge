7900097e0c0ac8c495c5dec36738c091e22a3113
Hariom Chouhan <hariomchouhan430@gmail.com>
2023-08-19 22:04:27 +0530

    remove unused code

--- day-46/src/App.js
+++ day-46/src/App.js
@@ -2,11 +2,6 @@ import React, { useState } from "react";
 import NameComponent, { FullNameComponent } from "./NameComponent";
 
 function App() {
-  const [state, setState] = useState(false);
-
-  const changeHandler = () => {
-    setState((prev) => !prev);
-  };
   return (
     <div className="w-full h-[100%] overflow-hidden flex flex-col justify-center items-center relative">
       <div className="absolute top-0 py-3 bg-cyan-500 w-full text-center font-bold text-4xl shadow-md text-white">
